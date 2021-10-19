58f8c20cc3d996539832715f7acc48cacd2685e4
Rishit21-oss <81700717+Rishit21-oss@users.noreply.github.com>
2021-10-19 20:36:33 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -15,7 +15,11 @@ recognition.onresult = function (event){
     var content= event.results[0][0].transcript;
     document.getElementById("textbox").innerHTML=content;
     console.log(content);
+    if(content =="take my selfie")
+    {
+    console.log("take my selfie");
     speak();
+    }  
 }
 
 function speak(){
@@ -25,12 +29,33 @@ function speak(){
     var utterThis = new SpeechSynthesisUtterance(speak_data);
     synth.speak(utterThis);
     Webcam.attach(camera);
+
+    setTimeout(function(){
+
+        take_snapshot();
+        save();
+    }, 5000);
 }
 
 camera = document.getElementById("camera");
 Webcam.set({
     width:360,
     height:250,
-    image_format: 'png',
-    png_quality: 90
+    image_format: 'jpeg',
+    jpeg_quality: 90
 });
+
+function take_snapshot(){
+
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'"/>';
+    });
+}
+
+function save(){
+
+    link= document.getElementById("link");
+    image = document.getElementById("selfie_image").src ;
+    link.herf = image;
+    link.click();
+}
